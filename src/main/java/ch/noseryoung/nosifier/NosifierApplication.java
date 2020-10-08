bd6355d9c286385c659dc36108c1da14aac68f33
Th3Dennis <62747122+Th3Dennis@users.noreply.github.com>
2020-10-08 10:53:29 +0200

    Initialized by Spring Initializr

---
+++ src/main/java/ch/noseryoung/nosifier/NosifierApplication.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.nosifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NosifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/noseryoung/nosifier/NosifierApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.nosifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
